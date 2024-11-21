2f5a740670ad7eadbc4db55fadaacebfea98d32d
retiredtab-gerrit <you@example.com>
2024-11-21 16:31:16 -0700

    gts3l-common: Need to build android.hardware.bluetooth@1.0 in  system namespace

--- gts3l.mk
+++ gts3l.mk
@@ -126,6 +126,8 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0 \
+    android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio-impl
 
 # Camera
